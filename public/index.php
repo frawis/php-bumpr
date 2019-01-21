f815590cb272b035924570be8fa82775e39d2ac3
Frank <frank.wissink@gmail.com>
2019-01-21 18:19:11 +0100

    add bulma

--- public/index.php
+++ public/index.php
@@ -1,15 +1,18 @@
-<?php
-$db = parse_url(getenv("DATABASE_URL"));
-$pdo = new PDO("pgsql:" . sprintf(
-	"host=%s;port=%s;user=%s;password=%s;dbname=%s",
-	$db["host"],
-	$db["port"],
-	$db["user"],
-	$db["pass"],
-	ltrim($db["path"], "/")
-));
-
-$query = "SELECT * FROM t_users";
-foreach ($pdo->query($query) as $row) {
-	echo $row['vorname'];
-}
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="x-ua-compatible" content="ie=edge">
+		<title>Bumpr</title>
+		<meta name="description" content="PHP Testanwendung">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="assets/css/style.css">
+	</head>
+	<body>
+		<!--[if lte IE 9]>
+			<p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
+		<![endif]-->
+		<h1 class="title">Bumpr</h1>
+		<p class="subtitle">Testanwendung</p>
+	</body>
+</html>
