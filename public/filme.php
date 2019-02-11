f43a364c9752c3ec27a984f1724d1b47d0eb819d
Frank <frank.wissink@gmail.com>
2019-02-11 23:54:45 +0100

    changed table layout

--- public/filme.php
+++ public/filme.php
@@ -12,35 +12,35 @@ $filme = $film->lesen();
 $count = $filme->rowCount();
 
 if ( $count > 0 ) {
-    ?>
-    <table class="table is-striped is-hoverable">
+	?>
+    <table class="table is-striped is-hoverable is-bordered is-fullwidth">
         <thead>
-            <tr>
-                <th>Titel</th>
-                <th>Jahr</th>
-                <th>Länge</th>
-                <th>Genre</th>
-                <th>Gesehen</th>
-            </tr>
+        <tr>
+            <th>Titel</th>
+            <th>Jahr</th>
+            <th>Länge</th>
+            <th>Genre</th>
+            <th>Gesehen</th>
+        </tr>
         </thead>
         <tbody>
-    <?php
-    while ($row = $filme->fetch(PDO::FETCH_ASSOC)){
-        extract($row);
-        ?>
-        <tr>
-            <td><?php echo $titel; ?></td>
-            <td><?php echo $jahr; ?></td>
-            <td><?php echo $laenge; ?></td>
-            <td><?php echo $genre; ?></td>
-            <td><?php echo $gesehen; ?></td>
-        </tr>
-        <?php
-    }
-    ?>
-    </tbody>
+		<?php
+		while ( $row = $filme->fetch( PDO::FETCH_ASSOC ) ) {
+			extract( $row );
+			?>
+            <tr>
+                <td><?php echo $titel; ?></td>
+                <td><?php echo $jahr; ?></td>
+                <td><?php echo $laenge; ?></td>
+                <td><?php echo $genre; ?></td>
+                <td><?php echo( $gesehen == 1 ? '+' : '-' ); ?></td>
+            </tr>
+			<?php
+		}
+		?>
+        </tbody>
     </table>
-    <?php
+	<?php
 } else {
 	?>
     <div class="notification is-danger">
